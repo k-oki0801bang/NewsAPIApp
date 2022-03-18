480fad0ca7bd89e391c3b2f387275f2fecdb5a96
Kouki <yamadakoki0124@icloud.com>
2022-03-18 11:00:32 +0900

    初期設定

---
+++ NewsAPIApp/View/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  NewsAPIApp
+//
+//  Created by 山田滉暁 on 2022/03/18.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
