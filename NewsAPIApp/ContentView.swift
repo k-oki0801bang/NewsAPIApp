7361a125fa28aea52bd4dc09b911fb9465d4541f
Kouki <yamadakoki0124@icloud.com>
2022-03-18 10:05:47 +0900

    Initial Commit

---
+++ NewsAPIApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewsAPIApp
+//
+//  Created by 山田滉暁 on 2022/03/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NewsAPIApp/NewsAPIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAPIAppApp.swift
+//  NewsAPIApp
+//
+//  Created by 山田滉暁 on 2022/03/18.
+//
+
+import SwiftUI
+
+@main
+struct NewsAPIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
